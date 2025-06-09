89aa369380a5361a31bc0807163d9f578a9f55c9
arun.mathew <arun.mathew@hrblock.com>
2025-06-09 12:04:42 +0530

    corrected note submit

--- src/app/add-notes/add-notes.ts
+++ src/app/add-notes/add-notes.ts
@@ -1,4 +1,9 @@
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteService } from '../services/note';
@@ -7,32 +12,31 @@ import { NoteService } from '../services/note';
   selector: 'app-add-notes',
   templateUrl: './add-notes.html',
   styleUrls: ['./add-notes.scss'],
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
 })
 export class AddNotes {
-  constructor(private noteService:NoteService) {}
-   noteForm = new FormGroup({
-    noteText: new FormControl('',Validators.required),
-    priority: new FormControl('',Validators.required),
-
+  constructor(private noteService: NoteService) {}
+  noteForm = new FormGroup({
+    noteText: new FormControl('', Validators.required),
+    priority: new FormControl('', Validators.required),
   });
-  priorities= [
+  priorities = [
     { value: 'critical', label: 'critical' },
     { value: 'high', label: 'high' },
-    { value: 'low', label: 'low' }
+    { value: 'low', label: 'low' },
   ];
   saveNote() {
     console.log('Note:', this.noteForm.value.noteText);
     console.log('Priority:', this.noteForm.value.priority);
+    
     this.noteService.addNote({
       noteText: this.noteForm.value.noteText ?? '',
       priority: this.noteForm.value.priority ?? '',
-      id: Date.now() // Using timestamp as a unique ID
+      id: Date.now(), // Using timestamp as a unique ID
     });
   }
 
   cancel() {
-    // this.note = '';
-    // this.priority = '';
+    this.noteForm.reset();
   }
 }
